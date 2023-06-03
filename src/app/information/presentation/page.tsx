2d9b4db71700867ed88350f2803a6fac2d2582ed
Rafael Brito <rafael.rbrito45@gmail.com>
2023-06-02 23:43:14 -0300

    minor build chaanges

--- src/app/information/presentation/page.tsx
+++ src/app/information/presentation/page.tsx
@@ -6,9 +6,9 @@ export const metadata = {
 export default async function Settings() {
   return(
     <div className='text-[#8F8CA8] mt-3 ml-5'>
-      <p className='pr-5 mb-3'>Hello! my name is Rafael, I'm a software developer, mostly working with front-end but also a back-end enthusiast.You will find everything about my projects and experiences on this page, fell free to contact me and take a look at my github projects.</p>
+      <p className='pr-5 mb-3'>Hello! my name is Rafael, I am a software developer, mostly working with front-end but also a back-end enthusiast.You will find everything about my projects and experiences on this page, fell free to contact me and take a look at my github projects.</p>
       <p className="pr-5 mb-3">One of my goals is to use technology to help people, be a reference when it comes to software development, and be the best version of myself everyday</p>
-      <p className="pr-5 mb-2">Knowledge is one of the best things you can aquire because no one can take it from you, speaking of knowledge, here is a short list of what I've been learning on my tech career:</p>
+      <p className="pr-5 mb-2">Knowledge is one of the best things you can aquire because no one can take it from you, speaking of knowledge, here is a short list of what I have been learning on my tech career:</p>
       <div className="flex justify-between">
         <ul className='list-disc ml-5 mb-4'>
           <li>React</li>
@@ -28,7 +28,7 @@ export default async function Settings() {
         />
       </div>
 
-      <p>Stay Relentless ✌️</p>
+      <p>Stay Relentless!</p>
 
     </div>
   )
--- src/app/vscode/extensions/page.tsx
+++
@@ -1,4 +0,0 @@
-export const metadata = {
-  title: 'VSCode Extensions',
-}
-
