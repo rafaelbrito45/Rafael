a46a89a24adde9fecd4a93c316b473e16b9165dd
Rafael Brito <rafael.rbrito45@gmail.com>
2023-06-05 15:39:58 -0300

    presentation text changes

--- src/app/information/presentation/page.tsx
+++ src/app/information/presentation/page.tsx
@@ -6,8 +6,8 @@ export const metadata = {
 export default async function Settings() {
   return(
     <div className='text-[#8F8CA8] mt-3 ml-5'>
-      <p className='pr-5 mb-3'>Hello! my name is Rafael, I am a software developer, mostly working with front-end but also a back-end enthusiast.You will find everything about my projects and experiences on this page, fell free to contact me and take a look at my github projects.</p>
-      <p className="pr-5 mb-3">One of my goals is to use technology to help people, be a reference when it comes to software development, and be the best version of myself everyday</p>
+      <p className='pr-5 mb-3'>Hello! my name is Rafael, I am a software developer, most of my experience is working with front-end but I also have a some experience with back-end.You will find everything about my projects and experiences on this page, fell free to contact me and take a look at my github projects.</p>
+      <p className="pr-5 mb-3">One of my goals is to use technology to help people, become a reference when it comes to software development, and be the best version of myself everyday.</p>
       <p className="pr-5 mb-2">Knowledge is one of the best things you can aquire because no one can take it from you, speaking of knowledge, here is a short list of what I have been learning on my tech career:</p>
       <div className="flex justify-between">
         <ul className='list-disc ml-5 mb-4'>
