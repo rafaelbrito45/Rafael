d8cc000ae10ba773593e337af357c3c9b7c3d7fb
Rafael Brito <rafael.rbrito45@gmail.com>
2023-11-10 15:27:42 -0300

    work xp update

--- src/app/experience/work/page.tsx
+++ src/app/experience/work/page.tsx
@@ -1,42 +1,43 @@
-import { CodePreview } from '@/components/CodePreview'
-import { getCodeBlockFromNotion } from '@/lib/notion-client'
-import { getNotionPagesId } from '@/lib/vercel-edge-config'
-import shiki from 'shiki'
+import { CodePreview } from "@/components/CodePreview";
+import { getCodeBlockFromNotion } from "@/lib/notion-client";
+import { getNotionPagesId } from "@/lib/vercel-edge-config";
+import shiki from "shiki";
 
 export const metadata = {
-  title: 'Experience',
-}
+  title: "Experience",
+};
 
 export default async function General() {
-  
-  return(
-    <div className='text-[#8F8CA8] ml-5'>
-        <div className='mb-5'>
-            <h2 className='text-xl font-bold'>Avenue Code</h2>
-            <p className='text-xs'>07/2021 - 05/2023</p>
-            <ul className='list-disc ml-5 mb-2'>
-                <li>Vue js</li>
-                <li>Adobe Experience Manager</li>
-                <li>Docker</li>
-                <li>Scrum</li>
-            </ul>
-            <p className="text-sm">Avenue Code Academy</p>
-            <ul className="list-disc ml-5">
-                <li>React</li>
-                <li>Jest</li>
-                <li>Vue</li>
-                <li>Vuex</li>
-            </ul>
-        </div>
+  return (
+    <div className="text-[#8F8CA8] ml-5">
+      <div className="mb-5">
+        <h2 className="text-xl font-bold">Avenue Code</h2>
+        <p className="text-xs">07/2021 - 05/2023</p>
+        <p className="text-sm mt-2">E-commerce for car brands </p>
+        <ul className="list-disc ml-5 mb-2">
+          <li>Vue js</li>
+          <li>Adobe Experience Manager</li>
+          <li>Docker</li>
+          <li>Vuex</li>
+          <li>Scrum</li>
+          <li>Jest</li>
+        </ul>
+        <p className="text-sm">Avenue Code's HR product</p>
+        <ul className="list-disc ml-5">
+          <li>React</li>
+          <li>Redux</li>
+          <li>Jest</li>
+        </ul>
+      </div>
 
-        <div>
-            <h2 className='text-xl font-bold'>It Beta2</h2>
-            <p className='text-xs'>03/2021 - 06/2021</p>
-            <ul className='list-disc ml-5 mb-2'>
-                <li>Angular</li>
-                <li>Flutter</li>
-            </ul>
-        </div>
+      <div>
+        <h2 className="text-xl font-bold">It Beta2</h2>
+        <p className="text-xs">03/2021 - 06/2021</p>
+        <ul className="list-disc ml-5 mb-2">
+          <li>Angular</li>
+          <li>Flutter</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/skills/techSkills/page.tsx
+++ src/app/skills/techSkills/page.tsx
@@ -1,14 +1,14 @@
-
 export const metadata = {
-  title: 'Tech skills',
-}
+  title: "Tech skills",
+};
 
 export default async function Settings() {
-  return(
-    <div className='text-[#8F8CA8] mt-3 ml-5'>
+  return (
+    <div className="text-[#8F8CA8] mt-3 ml-5">
       <div>
-        <ul className='list-disc ml-5 mb-2'>
+        <ul className="list-disc ml-5 mb-2">
           <li>React</li>
+          <li>React Native</li>
           <li>Angular</li>
           <li>Vue</li>
           <li>Node js</li>
@@ -20,5 +20,5 @@ export default async function Settings() {
         </ul>
       </div>
     </div>
-  )
+  );
 }
